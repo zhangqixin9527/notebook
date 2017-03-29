ca6694b2984bd83f98c83ebdcca3cdcc3fa5c6d6
zqx-git <531666378@qq.com>
2017-03-29 17:43:58 +0800

    init notebook project

---
+++ parent-utils/utils-common/src/main/java/com/hengba/utils/common/StringUtils.java
@@ -0,0 +1,19 @@
+/*
+ * 文件名称: StringUtils.java
+ * 版权信息: Copyright 2016-2020 technology Co., LTD. All right reserved.
+ * ----------------------------------------------------------------------------------------------
+ * 修改历史:
+ * ----------------------------------------------------------------------------------------------
+ * 修改原因: 新增
+ * 修改人员: Kevin
+ * 修改日期: 2017/3/29
+ * 修改内容: 
+ */
+package com.hengba.utils.common;
+
+/**
+ * @author Kevin created on 2017/3/29.
+ * @since framework 1.0
+ */
+public class StringUtils {
+}
